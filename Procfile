1a50ca3bf2e9ff67739281080082afdc19bf9ba7
Lucas Yepez <lucasyepez99@gmail.com>
2024-11-02 22:07:05 -0500

    edit

--- Procfile
+++
@@ -1,1 +0,0 @@
-web: java -jar target/Employee-Management-0.0.1-SNAPSHOT.jar
--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**") // Allow all endpoints
-                .allowedOrigins("http://localhost:5173") // Your React app's origin
+                .allowedOrigins("http://localhost:5174", "https://ecommerce-backend-1-yn41.onrender.com") // Allow both local and deployed frontend URLs
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
                 .allowCredentials(true); // Allow credentials if needed
