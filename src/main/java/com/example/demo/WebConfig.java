39c6f062515bbd33c1dd125136e9e180df48e8bf
Lucas Yepez <lucasyepez99@gmail.com>
2024-11-05 16:19:51 -0600

    debug cors

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -9,7 +9,7 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Allow all endpoints   https://ecommerce-backend-1-yn41.onrender.com/
+        registry.addMapping("/**") // Allow all endpoints
                 .allowedOrigins("http://127.0.0.1:5173","https://ecommerce-backend-1-yn41.onrender.com","https://oracle-ecommerce-j6m7iqrfb-lucas-projects-f61d5cb5.vercel.app") // Allow both local and deployed frontend URLs
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Allowed HTTP methods
                 .allowedHeaders("*") // Allow all headers
