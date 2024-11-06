62e672aebd5a42b6062c5e18779bc7299dbdea56
Lucas Yepez <lucasyepez99@gmail.com>
2024-11-06 14:01:56 -0600

    refactor post

--- src/main/java/com/example/demo/AuthController.java
+++ src/main/java/com/example/demo/AuthController.java
@@ -7,11 +7,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -24,7 +20,9 @@ public class AuthController {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
-    @CrossOrigin
+    @CrossOrigin(origins = "https://oracle-ecommerce-k7omklaqi-lucas-projects-f61d5cb5.vercel.app",
+            allowedHeaders = "*",
+            methods = {RequestMethod.POST, RequestMethod.OPTIONS})  // Allow POST and OPTIONS methods
     @PostMapping("/signup")
     public ResponseEntity<String> signup(@RequestBody SignupRequest signupRequest) {
         // Implement user creation logic
