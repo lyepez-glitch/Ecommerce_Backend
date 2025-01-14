54f373a5cb9d2498a7c9a542cf8dd8ffd973ada8
Lucas Yepez <lucasyepez99@gmail.com>
2025-01-13 20:38:51 -0600

    added cors

--- src/main/java/com/example/demo/WebConfig.java
+++ src/main/java/com/example/demo/WebConfig.java
@@ -1,4 +1,4 @@
-//package com.example.demo;
+package com.example.demo;//package com.example.demo;
 //
 //import org.springframework.context.annotation.Configuration;
 //import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -17,3 +17,20 @@
 //                .maxAge(3600); // Cache the CORS config for 1 hour
 //    }
 //}
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://oracle-ecommerce-6wsgotup1-lucas-projects-f61d5cb5.vercel.app") // Add your frontend URL here
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
